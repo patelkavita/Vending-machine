402cd81a1252a2e20bfa5bb9f6eabeb7cfa35b75
Kavita Patel <kavitapatel@Kavitas-MacBook-Pro.local>
2023-04-11 17:24:04 +0530

    Refactored code for optimum no. of coins

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,11 +1,28 @@
 const determineNoOfCoins = function(amount) {
-  const fiveRsCoins = Math.floor(amount / 5);
-  let amountRemaining = amount - (fiveRsCoins * 5);
-  const twoRsCoins = Math.floor(amountRemaining / 2);
-  amountRemaining = amountRemaining - (twoRsCoins * 2);
-  const oneRsCoins = amountRemaining % 2;
-  const totalCoins = fiveRsCoins+twoRsCoins + oneRsCoins;
-  return totalCoins ;
+  let coins = 0;
+  let amountRemaining = amount;
+
+  if (amountRemaining >= 10 ) {
+    coins += Math.floor(amountRemaining / 10); 
+    amountRemaining = amountRemaining - (coins * 10 );
+  } 
+
+  if (amountRemaining >= 5 ) {
+    coins += Math.floor(amountRemaining / 5); 
+    amountRemaining = amountRemaining - (coins * 5 );
+  }
+
+  if (amountRemaining >= 2 ) {
+    coins += Math.floor(amountRemaining / 2); 
+    amountRemaining = amountRemaining - (coins * 2 );
+  }
+
+  if (amountRemaining >= 1 ) {
+    coins += Math.floor(amountRemaining / 1); 
+    amountRemaining = amountRemaining - (coins * 1 );
+  }
+
+  return coins;
 }
 
 exports.determineNoOfCoins = determineNoOfCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -4,12 +4,15 @@ const assert = testing.assert;
 
 
 const testVendingMachine = function() {
-  assert(vendingMachine.determineNoOfCoins(1), 1, "should give 1 coin of rs 1 for amount : 1rs");
-  assert(vendingMachine.determineNoOfCoins(3), 2, "should give 2 coin of rs 1 and 2 for amount : 3rs");
-  assert(vendingMachine.determineNoOfCoins(2), 1, "should give 1 coin of rs. 2  for amount : 2rs");
-  assert(vendingMachine.determineNoOfCoins(5), 1, "should give 1 coin of rs 5 for amount : 5rs");
-  assert(vendingMachine.determineNoOfCoins(9), 3, "should give 2 coin of rs 2 and 5 for amount : 9rs");
-  assert(vendingMachine.determineNoOfCoins(10), 2, "should give 2 coin of rs 5 for amount : 10rs");
+  assert(1, vendingMachine.determineNoOfCoins(1), "should give 1 coin of rs 1 for amount : 1rs");
+  assert(2, vendingMachine.determineNoOfCoins(3), "should give 2 coin of rs 1 and 2 for amount : 3rs");
+  assert(1, vendingMachine.determineNoOfCoins(2), "should give 1 coin of rs.2 for amount : 2rs");
+  assert(1, vendingMachine.determineNoOfCoins(5), "should give 1 coin of rs 5 for amount : 5rs");
+  assert(3, vendingMachine.determineNoOfCoins(9), "should give 2 coin of rs 2 and 5 for amount : 9rs");
+  assert(1, vendingMachine.determineNoOfCoins(10), "should give 1 coin of rs 10 for amount : 10rs");
+  assert(2, vendingMachine.determineNoOfCoins(12), "should give 2 coin of rs 10 and 2 for amount : 12rs");
+  assert(3, vendingMachine.determineNoOfCoins(13), "should give 3 coin of rs 10, 2 and 1 for amount : 13rs");
+  assert(2, vendingMachine.determineNoOfCoins(15), "should give 2 coin of rs 10 and 5 for amount : 15rs");
 }
 
 testVendingMachine();
