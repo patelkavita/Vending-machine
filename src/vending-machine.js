b2f9a1196e031986096250097f0f7c5bffca217a
Kavita Patel <kavitapatel@Kavitas-MacBook-Pro.local>
2023-04-11 17:05:55 +0530

    Working for denomination for 1, 2 and 5

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,6 +1,11 @@
-const despenseCoins = function(amount) {
-
-  return amount / 1 ;
+const determineNoOfCoins = function(amount) {
+  const fiveRsCoins = Math.floor(amount / 5);
+  let amountRemaining = amount - (fiveRsCoins * 5);
+  const twoRsCoins = Math.floor(amountRemaining / 2);
+  amountRemaining = amountRemaining - (twoRsCoins * 2);
+  const oneRsCoins = amountRemaining % 2;
+  const totalCoins = fiveRsCoins+twoRsCoins + oneRsCoins;
+  return totalCoins ;
 }
 
-exports.despenseCoins = despenseCoins;
+exports.determineNoOfCoins = determineNoOfCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -4,8 +4,12 @@ const assert = testing.assert;
 
 
 const testVendingMachine = function() {
-  assert(vendingMachine.despenseCoins(1), 1, "should give 1 coin for 1rs. amount");
-  assert(vendingMachine.despenseCoins(3), 3, "should give 3 coin for 3rs. amount");
+  assert(vendingMachine.determineNoOfCoins(1), 1, "should give 1 coin of rs 1 for amount : 1rs");
+  assert(vendingMachine.determineNoOfCoins(3), 2, "should give 2 coin of rs 1 and 2 for amount : 3rs");
+  assert(vendingMachine.determineNoOfCoins(2), 1, "should give 1 coin of rs. 2  for amount : 2rs");
+  assert(vendingMachine.determineNoOfCoins(5), 1, "should give 1 coin of rs 5 for amount : 5rs");
+  assert(vendingMachine.determineNoOfCoins(9), 3, "should give 2 coin of rs 2 and 5 for amount : 9rs");
+  assert(vendingMachine.determineNoOfCoins(10), 2, "should give 2 coin of rs 5 for amount : 10rs");
 }
 
 testVendingMachine();
