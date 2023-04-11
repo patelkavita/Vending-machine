4cc478e892d1910f28a4ee8d74c61d4ea1cffdcf
Kavita Patel <kavitapatel@Kavitas-MacBook-Pro.local>
2023-04-11 18:21:30 +0530

    Refactored code for optimum no. of coins using loop

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,28 +1,17 @@
 const determineNoOfCoins = function(amount) {
+  const denominations = [10, 5, 2, 1];
   let coins = 0;
   let amountRemaining = amount;
+  let totalNoOfCoins = 0;
 
-  if (amountRemaining >= 10 ) {
-    coins += Math.floor(amountRemaining / 10); 
-    amountRemaining = amountRemaining - (coins * 10 );
-  } 
-
-  if (amountRemaining >= 5 ) {
-    coins += Math.floor(amountRemaining / 5); 
-    amountRemaining = amountRemaining - (coins * 5 );
+  for(let values = 0;  values < denominations.length; values++) {
+    if (amountRemaining >= denominations[values]) {
+      coins = Math.floor(amountRemaining / denominations[values]); 
+      totalNoOfCoins = totalNoOfCoins + coins;
+      amountRemaining = amountRemaining - (coins * denominations[values]);
+    } 
   }
 
-  if (amountRemaining >= 2 ) {
-    coins += Math.floor(amountRemaining / 2); 
-    amountRemaining = amountRemaining - (coins * 2 );
-  }
-
-  if (amountRemaining >= 1 ) {
-    coins += Math.floor(amountRemaining / 1); 
-    amountRemaining = amountRemaining - (coins * 1 );
-  }
-
-  return coins;
+  return totalNoOfCoins;
 }
-
 exports.determineNoOfCoins = determineNoOfCoins;
