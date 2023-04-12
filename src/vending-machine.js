e6484557d61416a3b8d3b21a6e22835adfcb774d
Kavita <patelkavita@users.noreply.github.com>
2023-04-12 08:49:50 +0530

    Added functionality to determine no. of coins, when denomination is provided in assending order

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,10 +1,10 @@
-const determineNoOfCoins = function(amount) {
-  const denominations = [10, 5, 2, 1];
+const determineNoOfCoins = function(amount,denominations) {
+  //const denominations = [10, 5, 2, 1];
   let coins = 0;
   let amountRemaining = amount;
   let totalNoOfCoins = 0;
 
-  for(let values = 0;  values < denominations.length; values++) {
+  for(let values = denominations.length-1;  values >=0; values--) {
     if (amountRemaining >= denominations[values]) {
       coins = Math.floor(amountRemaining / denominations[values]); 
       totalNoOfCoins = totalNoOfCoins + coins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -1,18 +1,23 @@
 const vendingMachine = require('./../src/vending-machine.js');
 const testing = require('./../lib/testing.js');
-const assert = testing.assert;
 
+const assert = testing.assert;
+const printHeadLine = testing.printHeadLine;
+const determineNoOfCoins = vendingMachine.determineNoOfCoins;
 
 const testVendingMachine = function() {
-  assert(1, vendingMachine.determineNoOfCoins(1), "should give 1 coin of rs 1 for amount : 1rs");
-  assert(2, vendingMachine.determineNoOfCoins(3), "should give 2 coin of rs 1 and 2 for amount : 3rs");
-  assert(1, vendingMachine.determineNoOfCoins(2), "should give 1 coin of rs.2 for amount : 2rs");
-  assert(1, vendingMachine.determineNoOfCoins(5), "should give 1 coin of rs 5 for amount : 5rs");
-  assert(3, vendingMachine.determineNoOfCoins(9), "should give 2 coin of rs 2 and 5 for amount : 9rs");
-  assert(1, vendingMachine.determineNoOfCoins(10), "should give 1 coin of rs 10 for amount : 10rs");
-  assert(2, vendingMachine.determineNoOfCoins(12), "should give 2 coin of rs 10 and 2 for amount : 12rs");
-  assert(3, vendingMachine.determineNoOfCoins(13), "should give 3 coin of rs 10, 2 and 1 for amount : 13rs");
-  assert(2, vendingMachine.determineNoOfCoins(15), "should give 2 coin of rs 10 and 5 for amount : 15rs");
+  printHeadLine("\nTesting for determineNoOfCoins");
+  assert(0, determineNoOfCoins(0, [1, 2]), "should give 0 coin for amount 0");
+  assert(1, determineNoOfCoins(1, [1, 2]), "should give total 1 coin of rs 1 for amount : 1rs");
+  assert(1, determineNoOfCoins(2, [1, 2]), "should give total 1 coin of rs.2 for amount : 2rs");
+  assert(2, determineNoOfCoins(3, [1, 2]), "should give total 2 coin of rs 2 and 1 for amount : 3rs");
+  assert(7, determineNoOfCoins(13, [1, 2]), "should give total 7 coin of rs 2 and 1 for amount : 13rs");
+  assert(1, determineNoOfCoins(5, [1, 2, 5]), "should give total 1 coin of rs 5 for amount : 5rs");
+  assert(3, determineNoOfCoins(15, [5]), "should give total 3 coin of rs 5 for amount : 15rs");
+  assert(2, determineNoOfCoins(10, [1, 2, 5]), "should give total 2 coin of rs 5 for amount : 10rs");
+  assert(3, determineNoOfCoins(9, [1, 2, 5, 10]), "should give total 3 coin of rs 5 and 2 for amount : 9rs");
+  assert(2, determineNoOfCoins(12, [1, 2, 10]), "should give total 2 coin of rs 10 and 2 for amount : 12rs");
 }
 
 testVendingMachine();
+testing.getSummary();
