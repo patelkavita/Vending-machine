08ca3295e3533bcf97a4df6d3694fb6c50aaed8a
Kavita <patelkavita@users.noreply.github.com>
2023-04-12 21:04:26 +0530

    Added sorting function and vending-machine is updated for any denomination and for any amount

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,5 +1,5 @@
 const determineNoOfCoins = function(amount,denominations) {
-  //const denominations = [10, 5, 2, 1];
+  const denominationsArray = maxSort(denominations); 
   let coins = 0;
   let amountRemaining = amount;
   let totalNoOfCoins = 0;
@@ -15,3 +15,34 @@ const determineNoOfCoins = function(amount,denominations) {
   return totalNoOfCoins;
 }
 exports.determineNoOfCoins = determineNoOfCoins;
+
+/*************************************************************************/
+
+const ejectAt = function(index,numbers) {
+  numbers = numbers.slice(0,index).concat(numbers.slice(index+1));
+
+  return numbers;
+}
+
+const maxSort = function(arrayToSort) {
+  let sortedArray = [];
+  let newArray = arrayToSort;
+  let length = arrayToSort.length;
+
+  for (let i = 0; i < arrayToSort.length; i++) {
+    let max = newArray[0];
+
+    for(let index = 0; index < length ; index++) {
+      max = Math.max(max, newArray[index]);
+    }
+    sortedArray.push(max);
+    newArray = ejectAt(newArray.indexOf(max), newArray);
+    length--;
+  }
+  return sortedArray;
+}
+
+//console.log(maxSort([1,3,6,2]));
+
+exports.maxSort = maxSort;
+
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -4,6 +4,8 @@ const testing = require('./../lib/testing.js');
 const assert = testing.assert;
 const printHeadLine = testing.printHeadLine;
 const determineNoOfCoins = vendingMachine.determineNoOfCoins;
+const maxSort = vendingMachine.maxSort;
+const assertArray= testing.assertArray;
 
 const testVendingMachine = function() {
   printHeadLine("\nTesting for determineNoOfCoins");
@@ -18,7 +20,19 @@ const testVendingMachine = function() {
   assert(3, determineNoOfCoins(9, [1, 2, 5, 10]), "should give total 3 coin of rs 5 and 2 for amount : 9rs");
   assert(2, determineNoOfCoins(12, [1, 2, 10]), "should give total 2 coin of rs 10 and 2 for amount : 12rs");
   assert(4, determineNoOfCoins(13, [1, 4, 7]), "should give total 4 coin of rs 7,2 and 1 for amount : 13rs");
+  assert(4, determineNoOfCoins(13, [1, 7, 4]), "should give total 4 coin of rs 7,2 and 1 for amount : 13rs");
 }
 
+
+const testMaxSort = function() {
+  printHeadLine("\n Testing for maxSort");
+  assertArray([3,2,1], maxSort([2,1,3]), "Should give 3,2,1 for 2,1,3")
+  assertArray([2,1,1], maxSort([1,2,1]), "Should give 2,1,1 for 1,2,1")
+  assertArray([1,1,1], maxSort([1,1,1]), "Should give 1,1,1 for 1,1,1")
+  assertArray([4,3,2], maxSort([2,3,4]), "Should give 4,3,2 for 2,3,4")
+  assertArray([], maxSort([]), "Should give empty for empty array")
+
+}
+testMaxSort();
 testVendingMachine();
 testing.getSummary();
